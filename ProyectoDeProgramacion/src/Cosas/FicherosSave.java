87ee4163f053e66e2f69ad5061be03bb34dfe2dd
Dario <dcondeojeda@gmail.com>
2019-05-17 20:43:10 +0200

    update clase FicherosSave

--- ProyectoDeProgramacion/src/Cosas/FicherosSave.java
+++ ProyectoDeProgramacion/src/Cosas/FicherosSave.java
@@ -1,5 +1,70 @@
 package Cosas;
 
-public class FicherosSave {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FicherosSave {
+	public void exportar(Partida partida, String nombre) {
+		try {
+			FileWriter writer = new FileWriter("src/Saves/"+nombre+".csv");
+			
+			String texto = "";
+			texto += partida.getUsuario();
+			texto += ";";
+			texto += Integer.toString(partida.getDinero());
+			texto += ";";
+			texto += Integer.toString(partida.getPuntuaciontotal());
+			texto += ";";
+			texto += Integer.toString(partida.getTiempototal());
+			texto += ";";
+			texto += Integer.toString(partida.getStandgalletitas());
+			texto += ";";
+			texto += Integer.toString(partida.getVendedorgalletitas());
+			texto += ";";
+			texto += Integer.toString(partida.getSupermercadogalletitas());
+			texto += ";";
+			texto += Integer.toString(partida.getMercadillogalletitas());
+			texto += ";";
+			texto += Integer.toString(partida.getFabricagalletitas());
+			texto += ";";
+			texto += Integer.toString(partida.getStandpizza());
+			texto += ";";
+			texto += Integer.toString(partida.getVendedorpizza());
+			texto += ";";
+			texto += Integer.toString(partida.getSupermercadopizza());
+			texto += ";";
+			texto += Integer.toString(partida.getMercadillopizza());
+			texto += ";";
+			texto += Integer.toString(partida.getFabricapizza());
+			texto += ";";
+			texto += Integer.toString(partida.getStandpanini());
+			texto += ";";
+			texto += Integer.toString(partida.getVendedorpanini());
+			texto += ";";
+			texto += Integer.toString(partida.getSupermercadopanini());
+			texto += ";";
+			texto += Integer.toString(partida.getMercadillopanini());
+			texto += ";";
+			texto += Integer.toString(partida.getFabricapanini());
+			texto += ";";
+			texto += Integer.toString(partida.getStandnoodles());
+			texto += ";";
+			texto += Integer.toString(partida.getVendedornoodles());
+			texto += ";";
+			texto += Integer.toString(partida.getSupermercadonoodles());
+			texto += ";";
+			texto += Integer.toString(partida.getMercadillonoodles());
+			texto += ";";
+			texto += Integer.toString(partida.getFabricanoodles());
+			
+			writer.write(texto);
+			writer.close();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void importar() {
+		
+	}
 }
